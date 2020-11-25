4d89ff9a62f13d9cba218fca6de2aaaaede9cf9b
walfud <hi@walfud.com>
2020-11-25 19:25:36 +0800

    todo: MyHistoryRepositoryImpl

--- server/src/main/kotlin/com/walfud/bugfree/server/history/HistoryRepository.kt
+++ server/src/main/kotlin/com/walfud/bugfree/server/history/HistoryRepository.kt
@@ -1,12 +1,16 @@
 package com.walfud.bugfree.server.history
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Pageable
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.LocalDateTime
 
-interface HistoryRepository : ReactiveCrudRepository<DbHistory, String> {
+interface HistoryRepository : ReactiveCrudRepository<DbHistory, String>, MyHistoryRepository {
     @Modifying
     @Query("""
                 INSERT INTO `history`
@@ -15,4 +19,15 @@ interface HistoryRepository : ReactiveCrudRepository<DbHistory, String> {
                 (:id, :jenkinsId, :name, :branch, :ver, :buildType, :category, :content, :urlInner, :urlOuter, :result, :who, :extra, :createTime, :updateTime)
         """)
     fun insert(id: String, jenkinsId: String, name: String, branch: String, ver: String, buildType: String, category: String, content: String, urlInner: String, urlOuter: String, result: Int, who: String, extra: String?, createTime: LocalDateTime, updateTime: LocalDateTime): Mono<Int>
+}
+
+interface MyHistoryRepository {
+    fun findBy(ver: String?, buildType: String?, category: String?, pageable: Pageable): Flux<DbHistory>
+}
+class MyHistoryRepositoryImpl @Autowired constructor(
+        private val template: R2dbcEntityTemplate
+) : MyHistoryRepository {
+    override fun findBy(ver: String?, buildType: String?, category: String?, pageable: Pageable): Flux<DbHistory> {
+        TODO("IMPL")
+    }
 }
