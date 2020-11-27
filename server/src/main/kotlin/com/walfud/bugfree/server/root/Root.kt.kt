363c2ca53c22741a7ff6707f306b819285509796
walfud <hi@walfud.com>
2020-11-27 20:06:07 +0800

    fix: avoid "/index" conflict

--- server/src/main/kotlin/com/walfud/bugfree/server/root/Root.kt.kt
+++ server/src/main/kotlin/com/walfud/bugfree/server/root/Root.kt.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class Root {
 
-    @GetMapping
+    @GetMapping("hello")
     fun hello(): String {
         return "hello, bugfree!"
     }
